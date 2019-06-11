a57d3408da2734d37707873bcb4a7436eec02ae7
Jedrzej Paulus <jmtpaulus@gmail.com>
2019-06-11 16:11:55 +0200

    initial commit'

---
+++ main.js
@@ -0,0 +1,10 @@
+alert('Elo zią!');
+console.log('Gierary hirr');
+
+const age = 34;
+const name  = 'Jędrzej';
+
+console.log(name)
+console.log(age);
+
+console.log(`Nazywam się ${name} i mam ${age} lat`);
