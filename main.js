01654016e3015da23c484a8c2d0448451adc4565
Jedrzej Paulus <jmtpaulus@gmail.com>
2019-06-18 09:15:59 +0200

    added small JS to display my name in green

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-// alert('Elo zią!');
+
+const age = 35;
+const name = 'Jędrzej';
+const diary = document.querySelector ('.diary__header--js');
+diary.innerHTML = `Nazywam się ${name} i mam ${age} lat`;
+
+ // alert('Elo zią!');
 // console.log('Gierary hirr');
 
-// const age = 34;
-// const name  = 'Jędrzej';
+
 
 // console.log(name)
 // console.log(age);
