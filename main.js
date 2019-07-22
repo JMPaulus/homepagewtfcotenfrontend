b82858ab721552ac11373a15d6f4c4ad72787a84
Jedrzej Paulus <jmtpaulus@gmail.com>
2019-07-22 08:50:17 +0200

    made a button, tomorrow will handle it better

--- main.js
+++ main.js
@@ -2,40 +2,49 @@
 const age = 34;
 const name = 'Jędrzej';
 const diary = document.querySelector('.diary__header--js');
-diary.innerHTML = `Nazywam się ${name} i mam ${age} lat`;
+diary.innerHTML = `Nazywam się ${name} i mam ${age} lata`;
 
-if (age >= 35) {
-    console.log("trzydzieści pięć lub mniej")
-}
-else if (age <= 35 && age >= 30) {
-    console.log('Między 30 a 35')
-}
-else {
-    console.log('nie trzydzieści pięć')
-};
+const button = document.querySelector('.header__button--js');
+console.log(button)
 
-switch (age) {
-    case 30:
-        console.log('Masz 30 lat');
-
-        break;
-    case 20:
-        console.log('Masz 20 lat');
-        break;
-    default:
-        console.log(`Masz ${age} lat`)
-};
-
-let oldIndicator;
-if (age > 70) {
-    oldIndicator = 'yes';
-} else {
-    oldIndicator = 'no';
+function handleClick(){
+    console.log('działa')
 }
 
-// the same as above can be written below
-const amIOld = (age > 70) ? 'yes' : 'no';
-console.log(amIOld);
+button.addEventListener('click', handleClick);
+
+// if (age >= 35) {
+//     console.log("trzydzieści pięć lub mniej")
+// }
+// else if (age <= 35 && age >= 30) {
+//     console.log('Między 30 a 35')
+// }
+// else {
+//     console.log('nie trzydzieści pięć')
+// };
+
+// switch (age) {
+//     case 30:
+//         console.log('Masz 30 lat');
+
+//         break;
+//     case 20:
+//         console.log('Masz 20 lat');
+//         break;
+//     default:
+//         console.log(`Masz ${age} lat`)
+// };
+
+// let oldIndicator;
+// if (age > 70) {
+//     oldIndicator = 'yes';
+// } else {
+//     oldIndicator = 'no';
+// }
+
+// // the same as above can be written below
+// const amIOld = (age > 70) ? 'yes' : 'no';
+// console.log(amIOld);
 
 
 // alert('Elo zią!');
@@ -51,25 +60,25 @@ console.log(amIOld);
 
 // FUNCTIONS - REPETITON
 
-function calculate(x) {
-    x = x + 3;
-    console.log(x);
-    return x * 7;
-}
+// function calculate(x) {
+//     x = x + 3;
+//     console.log(x);
+//     return x * 7;
+// }
 
-console.log(calculate(2));
-const myCalculation = calculate(4);
-console.log(myCalculation)
+// console.log(calculate(2));
+// const myCalculation = calculate(4);
+// console.log(myCalculation)
 
 
-// Fat Arrow function
+// // Fat Arrow function
 
-const calculateFat = x => (x + 3) * 7;
+// const calculateFat = x => (x + 3) * 7;
 
-console.log("parametr to 2 a wynik to " + calculateFat(2));
+// console.log("parametr to 2 a wynik to " + calculateFat(2));
 
-const welcome = (name, age) => {
-    console.log(`Hej! Mam na imię ${name} i mam ${age} lat`);
-}
+// const welcome = (name, age) => {
+//     console.log(`Hej! Mam na imię ${name} i mam ${age} lat`);
+// }
 
-welcome('Jędrzej', 34);
+// welcome('Jędrzej', 34);
